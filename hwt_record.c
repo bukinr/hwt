fad4989ffd8d2c1ff2124f9a36d46cb27411b3a4
Ruslan Bukin <br@bsdpad.com>
2023-05-30 17:28:45 +0100

    add hwt_record file

---
+++ hwt_record.c
@@ -0,0 +1,106 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Ruslan Bukin <br@bsdpad.com>
+ *
+ * This work was supported by Innovate UK project 105694, "Digital Security
+ * by Design (DSbD) Technology Platform Prototype".
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/errno.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#include "hwt.h"
+#include "hwtvar.h"
+#include "cs.h"
+
+#include "libpmcstat/libpmcstat.h"
+
+int
+process_records(struct trace_context *tc)
+{
+	pmcstat_interned_string path;
+	struct pmcstat_image *image;
+	struct pmc_plugins plugins;
+	struct pmcstat_args args;
+	unsigned long addr;
+	struct hwt_record_get record_get;
+	int nentries;
+	int error;
+	int j;
+	int i;
+
+	memset(&plugins, 0, sizeof(struct pmc_plugins));
+	memset(&args, 0, sizeof(struct pmcstat_args));
+	args.pa_fsroot = "/";
+	nentries = 256;
+
+	tc->records = malloc(sizeof(struct hwt_record_user_entry) * nentries);
+
+	record_get.pid = tc->pid;
+	record_get.cpu_id = tc->cpu_id;
+	record_get.records = tc->records;
+	record_get.nentries = &nentries;
+
+	error = ioctl(tc->fd, HWT_IOC_RECORD_GET, &record_get);
+	if (error != 0) {
+		printf("RECORD_GET cpuid %d error %d entires %d\n", i, error, nentries);
+		return (error);
+	}
+
+	for (j = 0; j < nentries; j++) {
+		printf("  lib #%d: path %s addr %lx size %lx\n", j,
+		    tc->records[j].fullpath,
+		    (unsigned long)tc->records[j].addr,
+		    tc->records[j].size);
+
+		path = pmcstat_string_intern(tc->records[j].fullpath);
+		if ((image = pmcstat_image_from_path(path, 0,
+		    &args, &plugins)) == NULL)
+			return (-1);
+
+		if (image->pi_type == PMCSTAT_IMAGE_UNKNOWN)
+			pmcstat_image_determine_type(image, &args);
+
+		addr = (unsigned long)tc->records[j].addr & ~1;
+		addr -= (image->pi_start - image->pi_vaddr);
+		pmcstat_image_link(tc->pp, image, addr);
+		printf("image pi_vaddr %lx pi_start %lx pi_entry %lx\n",
+		    (unsigned long)image->pi_vaddr,
+		    (unsigned long)image->pi_start,
+		    (unsigned long)image->pi_entry);
+	}
+
+	return (0);
+}
